b85fe43254ad1b33bc7363ea629a81891ae5383b
Andreas Domberg <andom@posteo.de>
2024-07-14 00:33:57 +0200

    fix last dashapps_rootdir and metadata

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 metadata = {
     "title": "Abgeordnetengewissen",
     "route": "/bundestag/",
-    "thumbnail": "dashapps/bundestag/preview.png",
+    "thumbnail": "dashapps/stimmung/preview.png",
     "synopsis": "Das Abstimmverhalten der Bundestagsabgeordneten gegenüber ihrer Fraktion: Wie oft und bei welchen Gesetzesvorhaben haben die Abgeordneten mit ihrer Fraktion oder von ihr abweichend gestimmt? Interaktive Visualisierung mit Daten von Abgeordnetenwatch.de",
 }
