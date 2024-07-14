0b0cfe5797fc0eaf09f43abaf170616feb75fd35
Andreas Domberg <andom@posteo.de>
2024-07-14 10:53:51 +0200

    fix synopsis title

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 metadata = {
-    "title": "Abgeordnetengewissen",
+    "title": "Abstimmverhalten",
     "route": "/bundestag/",
     "thumbnail": "dashapps/bundestag/preview.png",
     "synopsis": "Das Abstimmverhalten der Bundestagsabgeordneten gegenüber ihrer Fraktion: Wie oft und bei welchen Gesetzesvorhaben haben die Abgeordneten mit ihrer Fraktion oder von ihr abweichend gestimmt? Interaktive Visualisierung mit Daten von Abgeordnetenwatch.de",
