a41ab5b8dff2639ce4b07a71f67563a83a317c0b
Andreas Domberg <andom@posteo.de>
2024-08-31 10:02:40 +0200

    adapt metadata to bilingual routes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 metadata = {
     "title": "Abstimmverhalten",
-    "route": "/bundestag/",
-    "thumbnail": "dashapps/bundestag/preview.png",
+    "route": "/de/bundestag/",
+    "thumbnail": "dashapps/de/bundestag/preview.png",
     "synopsis": "Das Abstimmverhalten der Bundestagsabgeordneten gegenüber ihrer Fraktion: Wie oft und bei welchen Gesetzesvorhaben haben die Abgeordneten mit ihrer Fraktion oder von ihr abweichend gestimmt? Interaktive Visualisierung mit Daten von Abgeordnetenwatch.de",
 }
