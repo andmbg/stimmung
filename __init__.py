7cd82cff02d58ec2a59ecbb0875cb32928712431
Andreas Domberg <andom@posteo.de>
2024-09-08 10:54:04 +0200

    bilingualize metadata

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
 metadata = {
-    "title": "Party Line Voting at Bundestag",
-    "route": "/en/bundestag/",
-    "thumbnail": "dashapps/en/bundestag/preview.png",
-    "synopsis": "Voting patterns of members of the Bundestag compared to their parliamentary group: How often do representatives vote in line with their party or against, an on what issues? Interactive visualization with data from the Abgeordenetenwatch NGO.",
+    "en": {
+        "title": "Party Line Voting at Bundestag",
+        "route": "/en/bundestag/",
+        "thumbnail": "dashapps/en/bundestag/preview.png",
+        "synopsis": "Voting patterns of members of the Bundestag compared to their parliamentary group: How often do representatives vote in line with their party or against, an on what issues? Interactive visualization with data from the Abgeordenetenwatch NGO.",
+    },
+    "de": {
+        "title": "Wahlverhalten im Bundestag",
+        "route": "/de/bundestag/",
+        "thumbnail": "dashapps/de/bundestag/preview.png",
+        "synopsis": "Abstimmungsmuster von Abgeordneten im Bundestag im Vergleich zu ihrer Fraktion: Wie oft stimmen Abgeordnete entlang der Fraktionslinie oder dagegen, und bei welchen Themen? Interaktive Visualisierung mit Daten von abgeordnetenwatch.de.",
+    }
 }
